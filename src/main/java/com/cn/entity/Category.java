20308966702cccca366ee24c4a759c657aa4a3c2
renjianwei <374431970@qq.com>
2021-01-04 20:14:48 +0800

    序列化

--- src/main/java/com/cn/entity/Category.java
+++ src/main/java/com/cn/entity/Category.java
@@ -7,12 +7,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="yx_category")
-public class Category {
+public class Category implements Serializable {
 
     @Id
     private String id;
